ce7d71a5125acda9a61a95efcfed5479ef82012b
Felix Xi <f-xi@onu.edu>
2024-10-24 12:18:55 -0400

    Complete countCharacter function

--- Lab8p1/Source.cpp
+++ Lab8p1/Source.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int countCharacter(string str);
+int main() {
+	bool Qcheck = true;
+	string input;
+	int charcount = 0;
+	while (Qcheck) {
+		cout << "Enter a string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q")  break;
+		charcount = countCharacter(input);
+		cout << charcount<<endl;
+	}
+	return 0;
+}
+
+
+
+int countCharacter(string str) {
+	int charactercount = 0;
+	charactercount = str.length();
+	return charactercount;
+}
